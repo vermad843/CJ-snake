54ff4d7004504b7a07932e19223909191b95741b
vermad843 <vishnuverma7524@gmail.com>
2019-10-21 08:06:14 +0530

    nested forloops

--- src/App.js
+++ src/App.js
@@ -2,10 +2,34 @@ import React, { Component } from 'react';
 
 
 class App extends Component {
+  constructor()  {
+    super();
+    const grid = [];
+
+    for (let row = 0; row < 20; row++) {
+      let cols = [];
+      for (let col = 0; col < 20; col++) {
+         cols.push({
+           row,
+           col
+         })        
+      }   
+      grid.push(cols);   
+    }
+
+    this.state = {
+      grid
+    }
+
+  }
   render() {
     return (
       <div className = "App">
-         
+         <section className = "grid">
+           {
+
+           }
+         </section>
       </div>
     );
   }
