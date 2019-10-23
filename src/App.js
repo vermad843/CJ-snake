75e85f0cd67a6cd903c4c66a071d830d85087bcc
vermad843 <vishnuverma7524@gmail.com>
2019-10-23 08:12:54 +0530

    moving snake

--- src/App.js
+++ src/App.js
@@ -20,45 +20,52 @@ class App extends Component {
 
     this.state = {
       grid,
-      direction : 'RIGHT',
-      apple : {
-       row : Math.floor(Math.random() * 20),
-       col : Math.floor(Math.random() * 20)       
-      },
+      apple : this.getRandomApple(),
       snake : {
         head : {
           row : 9,
           col : 9
-        }
-      },
-  };
-  }
-  componentDidMount()  {
-    document.onkeydown = this.onKeyDown;
+        },
+        velocity : {
+          x : 1,                          //when game starts it goes to the right
+          y : 0
+        },
+      }
+    }
   }
 
-  onKeyDown = (e) => {
-    e = e || window.event;
-    switch(e.keyCode) {
-      case 38:
-        this.setState({direction : 'UP'});
-        break;
-      case 40:
-       this.setState({direction : 'DOWN'});
-       break;
-      case 37:
-       this.setState({direction : 'LEFT'});
-       break;
-      case 39:
-       this.setState({direction : 'RIGHT'}); 
-       break;
-    }
-  };
-  
-   
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.gameLoop()
+    }, 1000);
+  }
+
+  getRandomApple = () => ({
+    row : Math.floor(Math.random() * 20),
+    col : Math.floor(Math.random() * 20),   
+  })
+
+ gameLoop = () => {
+   this.setState(({snake}) => {
+     const nextState = {
+       snake : {
+         ...snake,
+         head : {
+           row : snake.head.row + snake.velocity.y,
+           col : snake.head.col + snake.velocity.x
+         },
+       },
+     };
+     return nextState;
+   });
+   setTimeout(() => {
+    this.gameLoop()
+  }, 1000)
+ }
+ 
   isApple = (cell) => {
     const{apple} = this.state;
-  return   apple.row ===cell.row 
+  return   apple.row ===cell.row  
     && apple.col === cell.col
   }
 
@@ -68,12 +75,56 @@ class App extends Component {
     && snake.head.col === cell.col
   }
 
+  
+  setVelocity = (event) => {
+    if (event.keyCode === 38) { // up
+      this.setState(({snake}) => ({
+        snake: {
+          ...snake,
+          velocity: {
+            x: 0,
+            y: -1,
+          }
+        }
+      }))
+    } else if (event.keyCode === 40) {// down 
+      this.setState(({snake}) => ({
+        snake: {
+          ...snake,
+          velocity: {
+            x: 0,
+            y: 1,
+          }
+        }
+      }))
+    } else if (event.keyCode === 39)  {//right
+      this.setState(({snake}) => ({
+        snake: {
+          ...snake,
+          velocity: {
+            x: 1,
+            y: 0,
+          }
+        }
+      }))
+    } else if (event.keyCode === 37)  { // left
+      this.setState(({snake}) => ({
+        snake: {
+          ...snake,
+          velocity: {
+            x: -1,
+            y: 0,
+          }
+        }
+      }))
+    }
+  }
 
 
   render() {
     const { grid } = this.state;
     return (
-      <div className = "App">
+      <div onKeyPress = {this.setVelocity } className = "App">
         {
          <section className = "grid">
             {
@@ -82,9 +133,9 @@ class App extends Component {
                  return <div key ={`${cell.row} ${cell.col}`}
                   className = {`cell
                     ${
-                      this.isApple(cell)
-                      ? 'apple' : this.isHead(cell)
-                      ? 'head' : ''  }`}>
+                      this.isHead(cell)
+                      ? 'head' : this.isApple(cell)
+                      ? 'apple' : ''  }`}>
                 </div>
               })
             })
